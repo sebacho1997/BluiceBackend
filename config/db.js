502a0fbb83874f17af9f0062d7d67585275f891e
sebacho123 <danocito.lol@gmail.com>
2025-08-31 16:45:35 -0400

    ssl

--- config/db.js
+++ config/db.js
@@ -3,11 +3,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: String(process.env.DB_USER),
-  host: String(process.env.DB_HOST),
-  database: String(process.env.DB_NAME),
-  password: String(process.env.DB_PASSWORD),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false } // <--- Habilita SSL para Render
 });
 
 module.exports = pool;

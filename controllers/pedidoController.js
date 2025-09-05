ce16783ce3f2bead43da8389d453fa3e82138c06
sebacho123 <danocito.lol@gmail.com>
2025-09-05 11:15:13 -0400

    logs

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -276,8 +276,10 @@ async obtenerPedidosAsignados(req, res) {
 
 async updateProductPriceInPedido(req, res) {
   console.log('entro a actualizar el pedido');
-  console.log(req.body);
   const { pedido_id, producto_id, nuevoPrecio } = req.body;
+  console.log('pedidoid: '+ pedido_id);
+  console.log('productoid: '+producto_id );
+  console.log('nuevoprecio: '+nuevoPrecio );
   try {
     const updated = await Pedido.updateProductPriceInPedido(pedido_id, producto_id, nuevoPrecio);
     res.json({ success: true, updated });
